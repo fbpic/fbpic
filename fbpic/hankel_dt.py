ae4e55f2f4278f467046a02fbbb0e81845942902
Remi Lehe <rlehe@lbl.gov>
2015-02-26 16:20:06 -0800

    Changed the implementation of the Hankel Transform. It now works very well with d=0.5.

--- fbpic/hankel_dt.py
+++ fbpic/hankel_dt.py
@@ -35,8 +35,8 @@ from scipy.optimize import fsolve
 from utils import array_multiply
 
 # The list of available methods
-available_methods = [ 'FHT','QDHT',
-                     'MDHT(m+1,m)', 'MDHT(m-1,m)', 'MDHT(m,m)']
+available_methods = [ # 'FHT',
+                      'QDHT', 'MDHT(m+1,m)', 'MDHT(m-1,m)', 'MDHT(m,m)']
 
 class DHT(object) :
     """
@@ -50,7 +50,7 @@ class DHT(object) :
     >>> G = trans.transform(F)
     """
 
-    def __init__(self,p,N,rmax,method) :
+    def __init__(self, p, N, rmax, method, **kw ) :
         """
         Calculate the r (position) and nu (frequency) grid
         on which the transform will operate.
@@ -70,7 +70,9 @@ class DHT(object) :
         (The function is assumed to be zero at that point.)
 
         method : string
-        One of the available numerical implementations of the Hankel transform
+        The method used to calculate the Hankel transform
+        
+        kw : optional arguments to be passed in the case of the MDHT
         """
         
         # Check that the method is valid
@@ -81,15 +83,15 @@ class DHT(object) :
 
         # Call the corresponding initialization routine
         if self.method == 'FHT' :
-            self.FHT_init(p,N,rmax)
+            self.FHT_init(p, N, rmax)
         elif self.method == 'QDHT' :
-            self.QDHT_init(p,N,rmax)
+            self.QDHT_init(p, N, rmax)
         elif self.method == 'MDHT(m,m)' :
-            self.MDHT_init(p,N,rmax,m=p)
+            self.MDHT_init(p, N, rmax, m=p, **kw)
         elif self.method == 'MDHT(m-1,m)' :
-            self.MDHT_init(p,N,rmax,m=p+1)
+            self.MDHT_init(p, N, rmax, m=p+1, **kw)
         elif self.method == 'MDHT(m+1,m)' :
-            self.MDHT_init(p,N,rmax,m=p-1)
+            self.MDHT_init(p, N, rmax, m=p-1, **kw)
 
         
     def get_r(self) :
@@ -233,7 +235,7 @@ class DHT(object) :
         return(f)
 
 
-    def MDHT_init(self,p,N,rmax,m) :
+    def MDHT_init(self, p, N, rmax, m, d=None, Fw='symmetric') :
         """
         Calculate r and nu for the MDHT.
         Custom Hankel Transform, see the paper associated with FBPIC
@@ -247,32 +249,77 @@ class DHT(object) :
            Index of the nu grid on which to evaluate the Hankel
            transform. This can only be p-1, p or p+1 for the
            algorithm to work.
+
+        d : float, optional
+           Offset of the regularly-spaced radial grid, within one cell
+           If None, this uses the zeros of the Bessel function.
+           (nu_n = alpha_n/(2*pi*rmax), r_n = alpha_n*rmax/S
+
+        Fw : string, optional
+           Method to calculate the forward transformation
+           If 'symmetric', uses a symmetric formula for the inverse matrix
+           If 'inverse', inverses the forward matrix to find the inverse matrix
         
         Also store the auxilary matrices M and invM.
         """
         # Check that m has a valid value
         if (m in [p-1, p, p+1]) == False :
-            raise ValueError('m must be either %d, %d or %d, but is %d' \
-                             %(p-1,p,p+1,m))
-
+            raise ValueError(
+                'm must be either %d, %d or %d, but is %d'  %(p-1,p,p+1,m))
+        
+        # Register values
+        self.d = d
+        self.Fw =Fw
+        self.p = p
+        self.m = m
+        
         # Calculate the zeros of the Bessel function
-        zeros = jn_zeros(m,N+1)
-
-        # Calculate the grid
+        if m == 0 : # 0 is not a zero of the Bessel function
+            zeros = jn_zeros(m, N+1)
+        else : # 0 is a zero of the Bessel function
+            zeros = np.hstack( (np.array([0.]), jn_zeros(m, N)) )
         last_alpha = zeros[-1] # The N+1^{th} zero
         alphas = zeros[:-1]    # The N first zeros
+
+        # Calculate the grid
         self.N = N
         self.rmax = rmax
-        self.r = (rmax*1./N) * ( np.arange(N) + 0.3 )
         self.nu = ( 1./(2*np.pi*rmax) ) * alphas
+        if d is not None :
+            print ('Using uniform grid')
+            self.r = (rmax*1./N) * ( np.arange(N) + d )
+        else :
+            if m != p :
+                print('')
+                print('Using special formula')
+                k = int(N/4) 
+                A = alphas[k]
+                J = jn_zeros(m,N)[-1]
+                S = abs(2./jn( m-1, alphas[k]))*np.sqrt(
+                1 + ( jn( m-1, A*alphas[1:]/J )**2 / \
+                    jn( m-1, alphas[1:] )**2 ).sum() 
+                )
+                print('S = %f' %S)
+                print('Instead of S = %f' %last_alpha )
+            else :
+                S = last_alpha
+            self.r = rmax*alphas/S
 
         # Calculate and store the inverse matrix invM
-        denom = 1./( np.pi * rmax**2 * jn(m+1,alphas)**2 )
+        # (imposed by the condition that the modes give delta functions)
+        if p == m :
+            denom = 1./( np.pi * rmax**2 * jn(m+1,alphas)**2 )
+        else :
+            denom = 1./( np.pi * rmax**2 * jn(p, alphas)**2 )
         num = jn( p, 2*np.pi* self.r[:, np.newaxis]*self.nu[np.newaxis,:] )
         self.invM = num * denom[np.newaxis, :]
 
         # Calculate the matrix M
-        self.M = np.linalg.inv( self.invM )
+        if Fw == 'inverse' :
+            self.M = np.linalg.inv( self.invM )
+        if Fw == 'symmetric' :
+            num = jn( p, 2*np.pi* self.nu[:, np.newaxis]*self.r[np.newaxis,:] )
+            self.M = (2*np.pi*rmax**2/S)**2 * num * denom[ np.newaxis, :]
 
 
     def MDHT_transform( self, F, axis ) :
--- tests/test_hankel_dt.py
+++ tests/test_hankel_dt.py
@@ -9,14 +9,16 @@ from scipy.special import jn, jn_zeros
 from scipy.special import eval_genlaguerre
 import time
 
-def compare_Hankel_methods( f_analytic, g_analytic, p, Nz, Nr, axis, rmax, npts=1000 ) :
+def compare_Hankel_methods( f_analytic, g_analytic, p, Nz, Nr,
+                            axis, rmax, npts=1000, **kw ) :
     """
     Compare the discrete Hankel transform of f_analytic to the
     analytical result g_analytic.
 
-    This function allows to see the effect of the Hankel transform on a *2d array*,
-    where the Hankel transform is taken only along the axis `axis`, with length Nr.
-    Nothing happens along the other axis, which has length Nz.
+    This function allows to see the effect of the Hankel transform on a
+    *2d array*, where the Hankel transform is taken only along the axis
+    `axis`, with length Nr. Nothing happens along the other axis, which
+    has length Nz.
     """
 
     # Plot the analytic function and its Hankel transform
@@ -32,7 +34,7 @@ def compare_Hankel_methods( f_analytic, g_analytic, p, Nz, Nr, axis, rmax, npts=
     for method in available_methods :
 
         # Initialize transform
-        dht = DHT( p, Nr, rmax, method )
+        dht = DHT( p, Nr, rmax, method, **kw )
 
         # Calculate f and g on the natural grid
         if axis != 0 :
@@ -56,18 +58,18 @@ def compare_Hankel_methods( f_analytic, g_analytic, p, Nz, Nr, axis, rmax, npts=
         for iz in range(Nz) :
             if axis !=0 :
                 plt.subplot(121)
-                plt.plot( dht.r, f_dht.real[iz], 'o', label=method )
+                plt.plot( dht.r, f_dht[iz], 'o', label=method )
                 plt.subplot(122)
-                plt.plot( dht.nu, g_dht.real[iz], 'o', label=method )
+                plt.plot( dht.nu, g_dht[iz], 'o', label=method )
             else :
                 plt.subplot(121)
-                plt.plot( dht.r, f_dht.real[:,iz], 'o', label=method )
+                plt.plot( dht.r, f_dht[:,iz], 'o', label=method )
                 plt.subplot(122)
-                plt.plot( dht.nu, g_dht.real[:,iz], 'o', label=method )
+                plt.plot( dht.nu, g_dht[:,iz], 'o', label=method )
         
-        # Calculate the RMS error
-        error_f = np.sqrt( ((f_dht.real-f)**2).mean() )
-        error_g = np.sqrt( ((g_dht.real-g)**2).mean() )
+        # Calculate the maximum error
+        error_f = np.max( abs(f_dht-f) )
+        error_g = np.max( abs(g_dht-g) )
         Dt_ms = (t2-t1)/(2*Nz)*1.e3
 
         # Finalization of the plots
@@ -79,13 +81,13 @@ def compare_Hankel_methods( f_analytic, g_analytic, p, Nz, Nr, axis, rmax, npts=
         print(method + ' with %d points' % Nr )
         print('--------------------')
         print('  - Time per transform : %.3f ms' % Dt_ms )
-        print('  - RMS error on the Hankel transform : %.3e' % error_g )
-        print("""  - RMS error on the orginal function
+        print('  - Max error on the Hankel transform : %.3e' % error_g )
+        print("""  - Max error on the orginal function
         after back and forth transform : %.3e""" % error_f)
 
     plt.show()
         
-def compare_power_p( p, rcut, N, rmax ) :
+def compare_power_p( p, rcut, N, rmax, **kw ) :
     """
     Test the Hankel transforms for the test function :
     x -> (x/rcut)^p for x < rcut
@@ -94,16 +96,21 @@ def compare_power_p( p, rcut, N, rmax ) :
     x -> rcut J_{p+1}(2\pi rcut x) / x
     
     """
-
+    print('')
+    print('-------------------------------------------')
     print('Testing with power function, of exponent %d' %p )
+    print('-------------------------------------------')
     
     def power_p( x ) :
         return( np.where( x<rcut, (x/rcut)**p, 0. ) )
 
     def power_p_trans(x) :
-        return( rcut*jn( p+1, 2*np.pi*rcut*x) /x )  
+        ans = np.where( x == 0.,
+                        2*np.pi*rcut**2*jn(p,0)/(p+2),
+                        rcut*jn( p+1, 2*np.pi*rcut*x) /x )
+        return( ans )  
     
-    compare_Hankel_methods( power_p, power_p_trans, p, 1, N, -1, rmax )
+    compare_Hankel_methods( power_p, power_p_trans, p, 1, N, -1, rmax, **kw )
 
 def compare_laguerre_gauss( p, n, N, rmax ) :
     """
@@ -112,12 +119,14 @@ def compare_laguerre_gauss( p, n, N, rmax ) :
     where L_n^p is a Gauss-Laguerre polynomial
     x -> (-1)^n (2\pi) (2\pi x)^p L_n^p((2\pi x)^2) exp(-(2\pi x)^2/2)
 
-    See Cavanagh et al., IEEE TRANSACTIONS ON
-    ACOUSTICS, SPEECH, AND SIGNAL PROCESSING,
-    VOL. ASSP-27, NO. 4, AUGUST 1979 
+    See Cavanagh et al., IEEE Transactions on Acoustics, Speech,
+    and Signal Processing, vol. assp-27, no. 4, August 1979 
     """
 
+    print('')
+    print('-----------------------------------------------------')
     print('Testing with Laguerre-Gauss polynomial of order %d %d' %(n,p) )
+    print('-----------------------------------------------------')
     
     def laguerre_n_p( x ) :
         return( x**p * eval_genlaguerre( n, p, x**2 ) * np.exp(-x**2/2) )
@@ -127,30 +136,52 @@ def compare_laguerre_gauss( p, n, N, rmax ) :
                 eval_genlaguerre( n, p, (2*np.pi*x)**2 ) * \
                 np.exp(-(2*np.pi*x)**2/2) )
     
-    compare_Hankel_methods( laguerre_n_p, laguerre_n_p_trans, p,1,N,-1,rmax )
+    compare_Hankel_methods( laguerre_n_p, laguerre_n_p_trans, p,
+                            1, N, -1, rmax, **kw )
 
-def compare_bessel( p, m, n, N, rmax ) :
+def compare_bessel( p, m, n, N, rmax, **kw ) :
     """
     Test the Hankel transforms for the test function :
     x -> J_p( k_n^{m} x )
     """
 
+    print('')
+    print('-----------------------------------------------')
+    print('Testing with Bessel mode order %d, with %d-th k' %(p,n) )
+    print('-----------------------------------------------')
+    
     k = jn_zeros( m, n+1 )[-1]/rmax
     
     def bessel_n_p( x ) :
         return( jn(p, k*x) )
 
     def delta( x ) :
-        return( np.where( abs(x - k/(2*np.pi)) < 0.1,
+        if p == m :
+            return( np.where( abs(x - k/(2*np.pi)) < 0.1,
+                          np.pi*rmax**2*jn(m+1, k*rmax)**2 , 0.) )
+        if p != m :
+            return( np.where( abs(x - k/(2*np.pi)) < 0.1,
                           np.pi*rmax**2*jn(p, k*rmax)**2 , 0.) )
     
-    compare_Hankel_methods( bessel_n_p, delta, p,1,N,-1,rmax )
+    compare_Hankel_methods( bessel_n_p, delta, p, 1, N, -1, rmax, **kw )
     
 if __name__ == '__main__' :
 
-    for p in range(4) :
-        compare_power_p( p, 1, 200, 4 )
+    N = 200
+    rmax = 4
+    kw = { 'd' : 0.5, 'Fw' : 'inverse' }
+    
+    for p in range(2) :
+        compare_power_p( p, 1, N, rmax, **kw )
 
     for p in range(2) :
         for n in range(2) :
-            compare_laguerre_gauss( p, n, 200, 4 )
+            compare_laguerre_gauss( p, n, N, rmax )
+
+    for p in range(1) :
+        compare_bessel( p, p, int(N*0.3), N, rmax, **kw )
+        compare_bessel( p, p, int(N*0.9), N, rmax, **kw )
+
+    for p in range(1) :
+        compare_bessel( p, p+1, int(N*0.3), N, rmax, **kw )
+        compare_bessel( p, p+1, int(N*0.9), N, rmax, **kw )
