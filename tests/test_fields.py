8f153e24b341f1020b43c5765336b9425128f03c
Remi Lehe <rlehe@lbl.gov>
2015-03-02 19:22:45 -0800

    Finished the test for fields.py. The results show go agreement with the theory.

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -2,13 +2,14 @@
 This file tests the structures implemented in fields.py,
 by studying the propagation of a Gaussian beam in vacuum.
 """
-
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.constants import c
+from scipy.optimize import curve_fit
 from fbpic.fields import Fields
 
-def test_pulse( Nz, Nr, Nm, Lz, Lr, L_prop, N_step, w0, ctau, k0, a0 ) :
+def test_pulse( Nz, Nr, Nm, Lz, Lr, L_prop, Nt, w0, ctau, k0, a0 ) :
     """
     Propagate the beam over a distance L_prop in N_step,
     and extracts the waist and a0 at each step
@@ -29,7 +30,7 @@ def test_pulse( Nz, Nr, Nm, Lz, Lr, L_prop, N_step, w0, ctau, k0, a0 ) :
     L_prop : float
        The total propagation distance (in microns)
 
-    N_step : int
+    Nt : int
        The number of timesteps to take, to reach that distance
 
     w0 : float
@@ -53,10 +54,10 @@ def test_pulse( Nz, Nr, Nm, Lz, Lr, L_prop, N_step, w0, ctau, k0, a0 ) :
     """
 
     # Initialize the fields object
-    dt = L_prop/c * 1./N_pts
+    dt = L_prop/c * 1./Nt
     fld = Fields( Nz, Lz, Nr, Lr, Nm, dt)
     z0 = Lz/2
-    init_fields( fld, w, ctau, k0, z0, a0 )
+    init_fields( fld, w0, ctau, k0, z0, a0 )
 
     # Create the arrays to get the waist and a0
     w = np.zeros(Nt)
@@ -67,6 +68,7 @@ def test_pulse( Nz, Nr, Nm, Lz, Lr, L_prop, N_step, w0, ctau, k0, a0 ) :
     fld.interp2spect('B')
 
     # Loop over the iterations
+    print('Running the simulation...')
     for it in range(Nt) :
         # Advance the Maxwell equations
         fld.push()
@@ -74,7 +76,9 @@ def test_pulse( Nz, Nr, Nm, Lz, Lr, L_prop, N_step, w0, ctau, k0, a0 ) :
         fld.spect2interp('E')
         fld.spect2interp('B')
         # Fit the fields to find the waist and a0
-        a[it], w[it] = fit_fields( fld )
+        w[it], a[it] = fit_fields( fld )
+        # Show the progression bar
+        progression_bar(it, Nt)
 
     # Get the analytical solution
     z_prop = c*dt*np.arange(1, Nt+1)
@@ -84,11 +88,11 @@ def test_pulse( Nz, Nr, Nm, Lz, Lr, L_prop, N_step, w0, ctau, k0, a0 ) :
         
     # Plot the results
     plt.subplot(121)
-    plt.plot( z_prop, w, '-' )
+    plt.plot( z_prop, w, 'o' )
     plt.plot( z_prop, w_analytic, '--' )
     plt.subplot(122)
-    plt.plot( z_prop, a, '-' )
-    plt.plot( z_prop, a_analytic, '--' )
+    plt.plot( z_prop, a*w, 'o' )
+    plt.plot( z_prop, a_analytic*w_analytic, '--' )
     
     # Return a dictionary of the results
     return( { 'a' : a, 'w' : w, 'fld' : fld } )
@@ -125,13 +129,33 @@ def init_fields( fld, w, ctau, k0, z0, a0, m=1 ) :
     r = fld.interp[m].r
 
     # Initialize the fields with the right value and phase
-    profile = Gaussian_pulse( z, r, w, ctau, k0, z0 ) 
-    fld.interp[m].Er[:,:] = a0*profile
-    fld.interp[m].Et[:,:] = -1.j*a0*profile
-    fld.interp[m].Br[:,:] = 1.j*a0/c*profile
-    fld.interp[m].Bt[:,:] = a0/c*profile
+    profile = Gaussian_pulse( z, r, w, ctau, k0, z0, a0 ) 
+    fld.interp[m].Er[:,:] = profile
+    fld.interp[m].Et[:,:] = -1.j*profile
+    fld.interp[m].Br[:,:] = 1.j*1./c*profile
+    fld.interp[m].Bt[:,:] = 1./c*profile
+
+def Gaussian_transverse_profile( r, w, a ) :
+    """
+    Calculte the Gaussian transverse profile.
+
+    This is used both for the initialization and
+    for the fit of the fields
     
-def Gaussian_pulse( z, r, ) :
+    Parameters
+    ----------
+    r: 1darray
+       Represents the positions of the grid in r
+
+    w : float
+       The initial waist of the laser (in microns)
+
+    a : float
+       The a0 of the pulse
+    """
+    return( a*np.exp( -r**2/w**2 ) )
+    
+def Gaussian_pulse( z, r, w0, ctau, k0, z0, a0 ) :
     """
     Calculate the profile of a Gaussian beam.
     This is used to initialize the beam
@@ -144,7 +168,7 @@ def Gaussian_pulse( z, r, ) :
     r: 1darray
        Represents the positions of the grid in r
 
-    w : float
+    w0 : float
        The initial waist of the laser (in microns)
 
     ctau : float
@@ -155,6 +179,9 @@ def Gaussian_pulse( z, r, ) :
 
     z0 : float
        The position of the centroid on the z axis
+
+    a0 : float
+       The initial a0 of the pulse
        
     Return
     ------
@@ -162,26 +189,46 @@ def Gaussian_pulse( z, r, ) :
        which contains the values of the 
     
     """
-    longitudinal = np.exp( -(z-z0)**2/L**2 )*np.cos(k0*z)
-    transverse = np.exp( -r**2/w**2 )
+    longitudinal = np.exp( -(z-z0)**2/ctau**2 )*np.cos(k0*(z-z0))
+    transverse = Gaussian_transverse_profile( r, w0, a0 )
     profile = longitudinal[:,np.newaxis]*transverse[np.newaxis,:]
     
     return(profile)
 
-
     
-def fit_fields( fld ) :
+def fit_fields( fld, m=1 ) :
     """
     Extracts the waist and a0 of the pulse through a transverse Gaussian fit.
 
-    The laser oscillations are first averaged longitudinally
+    The laser oscillations are first averaged longitudinally.
 
     Parameters
     ----------
     fld : Fields object from fbpic
-    """
 
-if __name__ = '__main__' :
+    m : int, optional
+       The index of the mode to be fitted
+    """
+    # Average the laser oscillations longitudinally
+    laser_profile = np.sqrt( (abs( fld.interp[1].Er )**2).mean(axis=0) )
+    # Compensate the lower amplitude
+    laser_profile = np.sqrt(2)*laser_profile
+
+    # Do the fit
+    r = fld.interp[m].r
+    fit_result = curve_fit(Gaussian_transverse_profile, r,
+                           laser_profile, p0=np.array([1,1]) )
+
+    return( fit_result[0] )
+
+def progression_bar(i, Ntot, Nbars=60, char='-') :
+    "Shows a progression bar with Nbars"
+    nbars = int( i*1./Ntot*Nbars )
+    sys.stdout.write('\r[' + nbars*char )
+    sys.stdout.write((Nbars-nbars)*' ' + ']')
+    sys.stdout.flush()
+    
+if __name__ == '__main__' :
     
     # Simulation box
     Nz = 300
@@ -190,10 +237,13 @@ if __name__ = '__main__' :
     Lr = 40.
     Nm = 2
     # Laser pulse
-    w = 2.
-    ctau = 5.
+    w0 = 2.
+    ctau = 10.
     k0 = 2*np.pi/0.8
     a0 = 1.
+    # Propagation
+    L_prop = 200.
+    N_step = 100
     
-
+    fld = test_pulse( Nz, Nr, Nm, Lz, Lr, L_prop, N_step, w0, ctau, k0, a0 )
     
