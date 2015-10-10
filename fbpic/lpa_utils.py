1ea443c013e89db3fc71c6799af909b6523c9b91
Remi Lehe <rlehe@lbl.gov>
2015-10-09 17:54:27 -0700

    Add the BoostConverter class
    This class allows to convert quantities from the lab frame to the boosted
    frame.

--- fbpic/lpa_utils.py
+++ fbpic/lpa_utils.py
@@ -341,6 +341,111 @@ def get_space_charge_spect( spect, gamma ) :
     # Deduce the B field
     spect.Bp[:,:] = -0.5j*spect.kr * Az + spect.kz * Ap
     spect.Bm[:,:] = -0.5j*spect.kr * Az - spect.kz * Am
-    spect.Bz[:,:] = 1.j*spect.kr * Ap + 1.j*spect.kr * Am    
-    
-    
+    spect.Bz[:,:] = 1.j*spect.kr * Ap + 1.j*spect.kr * Am
+
+
+class BoostConverter( object ):
+    """
+    Class that converts the parameters of a simulation
+    from the lab frame to the boosted frame.
+    """
+
+    def __init__(self, gamma0):
+        """
+        Initialize the object by calculating the velocity of the
+        boosted frame.
+
+        Parameters
+        ----------
+        gamma0: float
+            Lorentz factor of the boosted frame
+        """
+        self.gamma0 = gamma0
+        self.beta0 = np.sqrt( 1 - 1./gamma0**2 )
+
+    def static_length( self, lab_frame_vars ):
+        """
+        Converts a list of lengths that correspond to static objects
+        (e.g. the plasma) from the lab frame to the boosted frame, i.e:
+        L' = L / gamma0
+        
+        Parameters
+        ----------
+        lab_frame_vars: list of floats
+            A list containing several length to be converted
+
+        Returns
+        -------
+        A list with same number of elements, with the converted quantities
+        """
+        boosted_frame_vars = []
+        for length in lab_frame_vars:
+            boosted_frame_vars.append( length/self.gamma0 )
+
+        return( boosted_frame_vars )
+        
+    def copropag_length( self, lab_frame_vars ):
+        """
+        Converts a list of lengths that correspond to copropagating objects
+        (e.g. the laser) from the lab frame to the boosted frame, i.e:
+        L' = L * gamma0 (1 + beta0)
+        
+        Parameters
+        ----------
+        lab_frame_vars: list of floats
+            A list containing several length to be converted
+
+        Returns
+        -------
+        A list with same number of elements, with the converted quantities
+        """
+        boosted_frame_vars = []
+        for length in lab_frame_vars:
+            boosted_frame_vars.append( length * self.gamma0*(1 + self.beta0) )
+
+        return( boosted_frame_vars )
+
+    def static_density( self, lab_frame_vars ):
+        """
+        Converts a list of densities that correspond to static objects
+        (e.g. the plasma) from the lab frame to the boosted frame, i.e:
+        n' = n * gamma0
+        
+        Parameters
+        ----------
+        lab_frame_vars: list of floats
+            A list containing several length to be converted
+
+        Returns
+        -------
+        A list with same number of elements, with the converted quantities
+        """
+        boosted_frame_vars = []
+        for dens in lab_frame_vars:
+            boosted_frame_vars.append( dens * self.gamma0 )
+
+        return( boosted_frame_vars )
+
+    def longitudinal_momentum( self, lab_frame_vars ):
+        """
+        Converts a list of momenta from the lab frame to the boosted frame:
+        u_z' = gamma0 * ( u_z - sqrt(1 + u_z**2) * beta0 )
+
+        Warning: The above formula assumes that the corresponding
+        particle has no transverse motion at all.
+
+        Parameters
+        ----------
+        lab_frame_vars: list of floats
+            A list containing several momenta to be converted
+
+        Returns
+        -------
+        A list with same number of elements, with the converted quantities
+        """
+        boosted_frame_vars = []
+        for u_z in lab_frame_vars:
+            g = np.sqrt( 1 + u_z**2 )
+            boosted_frame_vars.append( self.gamma0*( u_z - g*self.beta0 ) )
+
+        return( boosted_frame_vars )
