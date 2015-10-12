37a5248ba156ad3eb995aa7b2b02de5bb0b68e1d
Remi Lehe <rlehe@lbl.gov>
2015-10-12 08:50:03 -0700

    Added an example input file.

---
+++ docs/example_input/boosted_frame_sim.py
@@ -0,0 +1,138 @@
+"""
+This is a typical input script that runs a simulation of
+laser-wakefield acceleration using FBPIC.
+
+Usage
+-----
+- Modify the parameters below to suit your needs
+- Type "python -i lpa_sim.py" in a terminal
+- When the simulation finishes, the python session will *not* quit.
+    Therefore the simulation can be continued by running sim.step()
+
+Help
+----
+All the structures implemented in FBPIC are internally documented.
+Enter "print(fbpic_object.__doc__)" to have access to this documentation,
+where fbpic_object is any of the objects or function of FBPIC.
+"""
+
+# -------
+# Imports
+# -------
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.constants import c
+# Import the relevant structures in FBPIC
+from fbpic.main import Simulation
+from fbpic.lpa_utils import add_laser, BoostConverter
+from fbpic.moving_window import MovingWindow
+from fbpic.openpmd_diag import FieldDiagnostic, ParticleDiagnostic
+
+# ----------
+# Parameters
+# ----------
+
+# The simulation box
+Nz = 800         # Number of gridpoints along z
+zmax = 0.e-6     # Length of the box along z (meters)
+zmin = -40.e-6
+Nr = 100         # Number of gridpoints along r
+rmax = 20.e-6    # Length of the box along r (meters)
+Nm = 2           # Number of modes used
+# The simulation timestep
+dt = (zmax-zmin)/Nz/c   # Timestep (seconds)
+N_step = 400           # Number of iterations to perform
+
+# Boosted frame
+gamma0 = 4.
+
+# The particles
+p_zmin = 5.e-6  # Position of the beginning of the plasma (meters)
+p_zmax = 100.e-6 # Position of the end of the plasma (meters)
+p_rmin = 0.      # Minimal radial position of the plasma (meters)
+p_rmax = 18.e-6  # Maximal radial position of the plasma (meters)
+n_e = 5.e25      # The density in the labframe (electrons.meters^-3)
+p_nz = 4         # Number of particles per cell along z
+p_nr = 4         # Number of particles per cell along r
+p_nt = 4         # Number of particles per cell along theta
+uz_m = 0.        # Initial momentum of the electrons in the lab frame
+
+# The laser
+a0 = 4.          # Laser amplitude
+w0 = 5.e-6       # Laser waist
+ctau = 5.e-6     # Laser duration
+z0 = -10.e-6     # Laser centroid
+zfoc = 0.e-6     # Focal position
+lambda0 = 0.8e-6 # Laser wavelength
+
+# The moving window
+v_window = c       # Speed of the window
+ncells_zero = 10   # Number of cells over which the field is set to 0
+                   # at the left end of the simulation box
+ncells_damp = 10   # Number of cells over which the field is damped,
+                   # at the left of the simulation box, after ncells_zero
+                   # in order to prevent it from wrapping around.
+
+# The diagnostics
+diag_period = 10        # Period of the diagnostics in number of timesteps
+fieldtypes = [ "E", "rho", "B", "J" ]  # The fields that will be written
+
+def dens_func( z, r ):
+    """
+    Return the relative density with respect to n_e,
+    at the position z and r
+    (i.e. return a number between 0 and 1)
+    """
+    # Allocate relative density
+    n = np.ones_like(z)
+    n = np.where( z < p_zmin, 0., n )
+    n = np.where( z > p_zmax, 0., n )
+    
+    return(n)
+
+# -------------------------------------------
+# Perform a boost of the different quantities
+# -------------------------------------------
+boost = BoostConverter( gamma0 )
+p_zmin, p_zmax, zfoc = boost.static_length([ p_zmin, p_zmax, zfoc ])
+zmin, zmax, z0, dt = boost.copropag_length([ zmin, zmax, z0, dt ])
+ctau, lambda0 = boost.copropag_length([ ctau, lambda0 ])
+n_e, = boost.static_density([ n_e ])
+uz_m, = boost.longitudinal_momentum([ uz_m ])
+
+# -----------------------
+# Checking the parameters
+# -----------------------
+if p_nr%2 == 1 :
+    raise UserWarning("Running the simulation with an odd number \n"
+                      "of macroparticles may result in a very \n"
+                      "noisy simulation.")
+
+# ---------------------------
+# Carrying out the simulation
+# ---------------------------
+
+# Initialize the simulation object
+sim = Simulation( Nz, zmax, Nr, rmax, Nm, dt,
+    p_zmin, p_zmax, p_rmin, p_rmax, p_nz, p_nr, p_nt, n_e,
+    dens_func=dens_func, zmin=zmin, initialize_ions=True ) 
+
+# Add a laser to the fields of the simulation
+add_laser( sim.fld, a0, w0, ctau, z0, lambda0=lambda0, zf=zfoc )
+
+# Configure the moving window
+sim.moving_win = MovingWindow( sim.fld.interp[0],
+                               ncells_damp=ncells_damp,
+                               ncells_zero=ncells_zero,
+                               uz_m=uz_m, period=1 )
+
+# Add a field diagnostic
+sim.diags = [ FieldDiagnostic(diag_period, sim.fld, fieldtypes=fieldtypes),
+              ParticleDiagnostic(diag_period, {"electrons" : sim.ptcl[0]}) ]
+
+### Run the simulation
+print('\n Performing %d PIC cycles' % N_step) 
+sim.step( N_step )
+print('')
+
+
