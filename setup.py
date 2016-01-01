8506cc33527f4539ee4e6ceda5492836e887f127
Remi Lehe <remi.lehe@normalesup.org>
2015-12-31 16:20:00 -0800

    Activate "python setup.py test"
    Doing "python setup.py test" now works, and ignores the
    unautomated test. At this point the only test performed
    is stub, that does not test FBPIC.

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import fbpic # In order to extract the version number
 
 # Obtain the long description from README.md
 with open('README.md') as f :
     long_description = f.read()
 
+# Get the package requirements from the requirements.txt file
+with open('requirements.txt') as f:
+    install_requires = [ line.strip('\n') for line in f.readlines() ]
+
 setup(
     name='fbpic',
-    install_requires=['numpy', 'scipy', 'matplotlib',
-                      'pyfftw', 'h5py', 'datetime' ],
+    install_requires=install_requires,
     version=fbpic.__version__,
     author='Remi Lehe',
     author_email='remi.lehe@normalesup.org',
-    packages=['fbpic', 'fbpic.particles', 'fbpic.fields', 'fbpic.boundaries',
-              'fbpic.fields.spectral_transform', 'fbpic.openpmd_diag'],
+    packages = find_packages('./'),
     description='Fourier-Bessel Particle-In-Cell code',
     long_description = long_description,
     platforms='Linux, MacOSX',
-    url='http://bitbucket.org/remilehe/fbpic'
+    url='http://bitbucket.org/remilehe/fbpic',
+    tests_require=['pytest'],
+    setup_requires=['pytest-runner']
 )
---
+++ tests/test_example_docs_script.py
@@ -0,0 +1,14 @@
+"""
+This test file is part of FB-PIC (Fourier-Bessel Particle-In-Cell).
+
+It makes sure that the example input script in `docs/example_input`
+works properly.
+
+Usage:
+This file is meant to be run from the root directory of openPMD-viewer,
+by any of the following commands
+$ python tests/test_example_docs_script.py
+$ python setup.py test
+"""
+def test_stuff():
+    print 'Hi'
