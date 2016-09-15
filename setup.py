9e57d009635d06a4305869bf36f417eec136b5a7
Remi Lehe <rlehe@lbl.gov>
2016-09-15 10:33:40 -0700

    Updated tests and setup.py

--- setup.py
+++ setup.py
@@ -4,25 +4,33 @@ import fbpic # In order to extract the version number
 # Obtain the long description from README.md
 with open('README.md') as f :
     long_description = f.read()
-
 # Get the package requirements from the requirements.txt file
 with open('requirements.txt') as f:
     install_requires = [ line.strip('\n') for line in f.readlines() ]
-# pyfftw is not included in the requirements.txt since it causes
-# a bug when doing conda install --file requirements.txt
-install_requires.append('pyfftw')
 
 setup(
     name='fbpic',
-    install_requires=install_requires,
     version=fbpic.__version__,
-    author='Remi Lehe',
-    author_email='remi.lehe@normalesup.org',
-    packages = find_packages('./'),
     description='Fourier-Bessel Particle-In-Cell code',
-    long_description = long_description,
-    platforms='Linux, MacOSX',
-    url='http://bitbucket.org/remilehe/fbpic',
-    tests_require=['pytest'],
-    setup_requires=['pytest-runner']
-)
+    long_description=long_description,
+    maintainer='Remi Lehe',
+    maintainer_email='remi.lehe@normalesup.org',
+    license='BSD-3-Clause-LBNL',
+    packages=find_packages('./'),
+    tests_require=['pytest', 'openpmd_viewer'],
+    setup_requires=['pytest-runner'],
+    install_requires=install_requires,
+    platforms='any',
+    url='http://github.com/FBPIC/FBPIC',
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'],
+    )
