5d70606e396e41536a8126e9cc848009046243a6
Remi Lehe <remi.lehe@normalesup.org>
2016-10-30 10:07:42 -0700

    Completed the test on the Cherenkov instability

--- tests/test_boosted.py
+++ tests/test_boosted.py
@@ -30,7 +30,6 @@ from scipy.constants import c
 import matplotlib.pyplot as plt
 # Import the relevant structures in FBPIC
 from fbpic.main import Simulation
-from fbpic.openpmd_diag import FieldDiagnostic, ParticleDiagnostic
 
 # ----------
 # Parameters
@@ -40,19 +39,16 @@ use_cuda = True
 # Whether to show the plots of the instability
 show = True
 
-# Speed of galilean frame (set to 0 for a normal simulation)
-v_galilean = -0.999999*c
-
 # The simulation box
-Nz = 100         # Number of gridpoints along z
-zmax = 19.64     # Length of the box along z (meters)
-zmin = -19.64
-Nr = 50          # Number of gridpoints along r
-rmax = 19.64     # Length of the box along r (meters)
+Nz = 40         # Number of gridpoints along z
+zmax = 7.86     # Length of the box along z (meters)
+zmin = -7.86
+Nr = 20          # Number of gridpoints along r
+rmax = 7.86      # Length of the box along r (meters)
 Nm = 2           # Number of modes used
 # The simulation timestep
 dt = (zmax-zmin)/Nz/c   # Timestep (seconds)
-N_step = 100     # Number of iterations to perform
+N_step = 300     # Number of iterations to perform
 
 # The boost
 gamma_boost = 130.
@@ -64,8 +60,8 @@ p_zmax = zmax  # Position of the end of the plasma (meters)
 p_rmin = 0.    # Minimal radial position of the plasma (meters)
 p_rmax = rmax  # Maximal radial position of the plasma (meters)
 n_e = gamma_boost/(4*3.14*2.81e-15)
-p_nz = 2         # Number of particles per cell along z
-p_nr = 2         # Number of particles per cell along r
+p_nz = 2        # Number of particles per cell along z
+p_nr = 2        # Number of particles per cell along r
 p_nt = 4         # Number of particles per cell along theta
 
 # The moving window
@@ -77,51 +73,77 @@ v_window = c       # Speed of the window
 def get_Er_rms(sim):
     Er0 = sim.fld.interp[0].Er
     Er1 = sim.fld.interp[1].Er
-    Er_tot = np.mean( abs(Er0)**2 + abs(Er1)**2 )
-    return( np.sqrt( Er_tot ) )
+    Er_tot = np.sqrt( np.average( abs(Er0)**2 + abs(Er1)**2 ) )
+    return( Er_tot )
 
-def test_instability_standard( show=False ):
+def test_cherenkov_instability( show=False ):
     """
-    Run a simulation with the standard scheme and check 
-    that it is unstable
+    Run a simulation with the standard and Galilean scheme respectively
+    and check that the first one is unstable while the second one is stable
     """
-    # Initialize the simulation object
-    sim = Simulation( Nz, zmax, Nr, rmax, Nm, dt,
-        p_zmin, p_zmax, p_rmin, p_rmax, p_nz, p_nr, p_nt, n_e,
-        zmin=zmin, initialize_ions=True,
-        v_comoving=0, exchange_period=1, n_guard=5,
-        boundaries='periodic', use_cuda=use_cuda )
-
-    # Give a relativistic velocity to the particle, with some noise
-    np.random.seed(0)
-    sim.ptcl[0].uz[:] = uz_m + 1.e-10*np.random.normal(size=sim.ptcl[0].Ntot)
-    sim.ptcl[0].inv_gamma[:] = 1./np.sqrt( 1 + sim.ptcl[0].uz**2 )
-    sim.ptcl[1].uz[:] = uz_m + 1.e-10*np.random.normal(size=sim.ptcl[0].Ntot)
-    sim.ptcl[1].inv_gamma[:] = 1./np.sqrt( 1 + sim.ptcl[1].uz**2 )
+    # Dictionary to record the final value of E
+    slope_Erms = {}
     
-    # Perform the simulation; record the rms electric field every 50 timestep
-    Er_rms = np.zeros(int(N_step/10)+1)
-    t = np.zeros(int(N_step/10+1))
-    Er_rms[0] = get_Er_rms(sim)
-    t[0] += sim.time
-    for i in range(int(N_step/10)):
-        sim.step( 10, show_progress=False )
-        print('Checkpoint %d' %i)
-        Er_rms[i+1] = get_Er_rms(sim)
-        t[i+1] += sim.time
-        print('Calculated RMS')
+    for scheme in [ 'standard', 'galilean', 'pseudo-galilean']:
+
+        # Choose the correct parameters for the scheme
+        if scheme == 'standard':
+            v_comoving = 0.
+            use_galilean = False
+        else:
+            v_comoving = 0.9999*c
+            if scheme == 'galilean':
+                use_galilean = True
+            else:
+                use_galilean = False
+
+        # Initialize the simulation object
+        sim = Simulation( Nz, zmax, Nr, rmax, Nm, dt,
+            p_zmin, p_zmax, p_rmin, p_rmax, p_nz, p_nr, p_nt, n_e,
+            zmin=zmin, initialize_ions=True,
+            v_comoving=v_comoving, use_galilean=use_galilean,
+            n_guard=5, exchange_period=1,
+            boundaries='periodic', use_cuda=use_cuda )
         
-    # Check/plot the results
+        # Give a relativistic velocity to the particle, with some noise
+        sim.ptcl[0].uz[:] = uz_m
+        sim.ptcl[0].inv_gamma[:] = 1./np.sqrt( 1 + sim.ptcl[0].uz**2 )
+        sim.ptcl[1].uz[:] = uz_m
+        sim.ptcl[1].inv_gamma[:] = 1./np.sqrt( 1 + sim.ptcl[1].uz**2 )
+        
+        # Perform the simulation;
+        # record the rms electric field every 50 timestep
+        Er_rms = np.zeros(int(N_step/30)+1)
+        t = np.zeros(int(N_step/30+1))
+        Er_rms[0] = get_Er_rms(sim)
+        t[0] += sim.time
+        for i in range(int(N_step/30)):
+            sim.step( 30, show_progress=False )
+            print('Checkpoint %d' %i)
+            Er_rms[i+1] = get_Er_rms(sim)
+            t[i+1] += sim.time
+            print('Calculated RMS')
+        
+        # Check/plot the results
+        if show:
+            # Add a plot
+            plt.semilogy( t, Er_rms, '-', label=scheme )
+            plt.ylabel('RMS(Er)')
+            plt.xlabel('Time')
+        else:
+            # Registed the final value of the slope of the electric field
+            slope_Erms[scheme] = np.log( Er_rms[-1] ) - np.log(Er_rms[-2] )
+
     if show:
-        plt.plot( t, Er_rms, label='Standard scheme' )
-        plt.ylabel('RMS(Er)')
-        plt.xlabel('Time')
+        # Show the plot
+        plt.legend(loc=0)
+        plt.show()
+    else:
+        # Check that, in the standard case, the electric field is
+        # growing much faster, due to the Cherenkov instability
+        assert slope_Erms['standard'] > 4*slope_Erms['galilean']
+        assert slope_Erms['standard'] > 4*slope_Erms['pseudo-galilean']
 
-
-    
 if __name__ == '__main__':
 
-    test_instability_standard( show=show )
-    
-    if show==True:
-        plt.show()
+    test_cherenkov_instability( show=show )
