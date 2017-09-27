dcc2e125bf2409381d3d923c425624e4e8588558
Remi Lehe <remi.lehe@normalesup.org>
2017-09-27 11:48:00 +0200

    Corrected failing test

--- fbpic/lpa_utils/bunch.py
+++ fbpic/lpa_utils/bunch.py
@@ -479,7 +479,7 @@ def get_space_charge_fields( sim, ptcl, gamma, direction='forward') :
 
     # For consistency and diagnostics, redeposit the charge and current
     # of the full simulation (since the last step erased these quantities)
-    sim.deposit('rho')
+    sim.deposit('rho_prev')
     sim.deposit('J', exchange_J=True)
 
     print("Done.")
