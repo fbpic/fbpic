b66a40bb0fcc76dff02574ff773c29a351eae93b
Remi Lehe <remi.lehe@normalesup.org>
2017-10-23 22:23:37 -0700

    Added documentation and corrected plateform

--- fbpic/fields/spectral_transform/mkl_fft.py
+++ fbpic/fields/spectral_transform/mkl_fft.py
@@ -6,27 +6,29 @@ It allows the use of the MKL library for FFT.
 # For this reason, the corresponding copyright is reproduced here:
 
 # Copyright (c) 2016 Ivan Dokmanic, Robin Scheibler
-# Permission is hereby granted, free of charge, to any person obtaining a copy of
-# this software and associated documentation files (the "Software"), to deal in
-# the Software without restriction, including without limitation the rights to
-# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-# of the Software, and to permit persons to whom the Software is furnished to do
-# so, subject to the following conditions:
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to 
+# deal in the Software without restriction, including without limitation the 
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+# sell copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
 
 import sys
 import ctypes
 import numpy as np
 
 # Load the MKL Library for the current plateform
-if sys.platform == 'linux':
+if sys.platform in ['linux', 'linux2']:
     mkl = ctypes.CDLL('libmkl_rt.so')
 elif sys.platform == 'darwin':
     mkl = ctypes.CDLL('libmkl_rt.dylib')
 elif sys.platform == 'win32':
     mkl = ctypes.CDLL('libmkl_rt.dll')
+else:
+    raise ValueError("Unrecognized platform: %s" %sys.platform)
 
 # Define a set of flags that are passed to the MKL library
 # The values of these flags are copied from mkl_dfti.h
@@ -46,12 +48,24 @@ DFTI_NOT_INPLACE            = ctypes.c_int(44)
 
 class MKLFFT( object ):
     """
-    TODO
+    Minimal MKL FFT class that only performs the type of FFT relevant for FBPIC,
+    i.e. from complex128 to complex128, along the axis 0 of a 2D array
+
+    Note: the number of thread used is determined by the environment variable
+    MKL_NUM_THREADS
     """
 
     def __init__( self, a ):
         """
-        TODO
+        Initialize the descriptor of the MKL FFT.
+        The descriptor is then reused for each call to the methods
+        `transform` and `inverse_transform`
+
+        Parameters
+        ----------
+        a: 2darray of complex128
+            Array of the same shape as the ones that will later be
+            passed to the methods `transform` and `inverse_transform`
         """
 
         # Perform a few checks on the array type and shape
