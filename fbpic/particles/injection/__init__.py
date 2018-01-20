8e833b4da286c0a5896913478644c28ca99a5a51
Remi Lehe <remi.lehe@normalesup.org>
2018-01-20 11:52:45 -0500

    Implement ballistic injection of particle before a plane

---
+++ fbpic/particles/injection/__init__.py
@@ -0,0 +1,7 @@
+"""
+This file is part of the Fourier-Bessel Particle-In-Cell code (FB-PIC)
+It imports objects that handle continuous particle injection during
+the simulation.
+"""
+from .ballistic_before_plane import BallisticBeforePlane
+__all__ = ['BallisticBeforePlane']
