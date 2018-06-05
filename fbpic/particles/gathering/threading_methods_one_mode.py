908e7b5ba315253d20c427e821153a67452b391a
Pierre Pelletier <ppelletier@lbl.gov>
2018-06-05 11:37:05 -0700

    Fixed a small error in a yet unused method

--- fbpic/particles/gathering/threading_methods_one_mode.py
+++ fbpic/particles/gathering/threading_methods_one_mode.py
@@ -518,7 +518,8 @@ def gather_envelope_field_numba_cubic_one_mode(x, y, z,
                     invdz, zmin, Nz,
                     invdr, rmin, Nr,
                     a_m, grad_a_r_m, grad_a_t_m, grad_a_z_m, m,
-                    a, grad_a_x, grad_a_y, grad_a_z):
+                    a, grad_a_x, grad_a_y, grad_a_z,
+                    nthreads, ptcl_chunk_indices):
     """
     Gathering of the field a and grad_a using numba with multi-threading.
     Iterates over the particles, calculates the weighted amount
@@ -551,6 +552,12 @@ def gather_envelope_field_numba_cubic_one_mode(x, y, z,
         The relevant fields acting on the particles
         (is modified by this function)
 
+    nthreads : int
+        Number of CPU threads used with numba prange
+
+    ptcl_chunk_indices : array of int, of size nthreads+1
+        The indices (of the particle array) between which each thread
+        should loop. (i.e. divisions of particle array between threads)
     """
     # Gather the field per cell in parallel
     for nt in prange( nthreads ):
