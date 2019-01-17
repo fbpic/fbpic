885b506d890dfc8a3ad2ede7e3ed6750336f3899
Remi Lehe <remi.lehe@normalesup.org>
2019-01-17 14:55:21 -0800

    Further modifications of the shape functions

--- fbpic/particles/deposition/cuda_methods_one_mode.py
+++ fbpic/particles/deposition/cuda_methods_one_mode.py
@@ -11,6 +11,7 @@ import math
 from scipy.constants import c
 import numpy as np
 
+
 # -------------------------------
 # Particle shape Factor functions
 # -------------------------------
@@ -19,55 +20,63 @@ import numpy as np
 @cuda.jit(device=True, inline=True)
 def z_shape_linear(cell_position, index):
     iz = int(math.ceil(cell_position)) - 1
+    s = 0.
     if index == 0:
-        return iz+1.-cell_position
-    if index == 1:
-        return cell_position - iz
+        s = iz+1.-cell_position
+    elif index == 1:
+        s = cell_position - iz
+    return s
 
 @cuda.jit(device=True, inline=True)
 def r_shape_linear(cell_position, index):
     flip_factor = 1.
     ir = int(math.ceil(cell_position)) - 1
+    s = 0.
     if index == 0:
         if ir < 0:
             flip_factor = -1.
-        return flip_factor*(ir+1.-cell_position)
-    if index == 1:
-        return flip_factor*(cell_position - ir)
+        s = flip_factor*(ir+1.-cell_position)
+    elif index == 1:
+        s = flip_factor*(cell_position - ir)
+    return s
 
 # Cubic shapes
 @cuda.jit(device=True, inline=True)
 def z_shape_cubic(cell_position, index):
     iz = int(math.ceil(cell_position)) - 2
+    s = 0.
     if index == 0:
-        return (-1./6.)*((cell_position-iz)-2)**3
-    if index == 1:
-        return (1./6.)*(3*((cell_position-(iz+1))**3)-6*((cell_position-(iz+1))**2)+4)
-    if index == 2:
-        return (1./6.)*(3*(((iz+2)-cell_position)**3)-6*(((iz+2)-cell_position)**2)+4)
-    if index == 3:
-        return (-1./6.)*(((iz+3)-cell_position)-2)**3
+        s = (-1./6.)*((cell_position-iz)-2)**3
+    elif index == 1:
+        s = (1./6.)*(3*((cell_position-(iz+1))**3)-6*((cell_position-(iz+1))**2)+4)
+    elif index == 2:
+        s = (1./6.)*(3*(((iz+2)-cell_position)**3)-6*(((iz+2)-cell_position)**2)+4)
+    elif index == 3:
+        s = (-1./6.)*(((iz+3)-cell_position)-2)**3
+    return s
 
 @cuda.jit(device=True, inline=True)
 def r_shape_cubic(cell_position, index):
     flip_factor = 1.
     ir = int(math.ceil(cell_position)) - 2
+    s = 0.
     if index == 0:
         if ir < 0:
             flip_factor = -1.
-        return flip_factor*(-1./6.)*((cell_position-ir)-2)**3
-    if index == 1:
+        s = flip_factor*(-1./6.)*((cell_position-ir)-2)**3
+    elif index == 1:
         if ir+1 < 0:
             flip_factor = -1.
-        return flip_factor*(1./6.)*(3*((cell_position-(ir+1))**3)-6*((cell_position-(ir+1))**2)+4)
-    if index == 2:
+        s = flip_factor*(1./6.)*(3*((cell_position-(ir+1))**3)-6*((cell_position-(ir+1))**2)+4)
+    elif index == 2:
         if ir+2 < 0:
             flip_factor = -1.
-        return flip_factor*(1./6.)*(3*(((ir+2)-cell_position)**3)-6*(((ir+2)-cell_position)**2)+4)
-    if index == 3:
+        s = flip_factor*(1./6.)*(3*(((ir+2)-cell_position)**3)-6*(((ir+2)-cell_position)**2)+4)
+    elif index == 3:
         if ir+3 < 0:
             flip_factor = -1.
-        return flip_factor*(-1./6.)*(((ir+3)-cell_position)-2)**3
+        s = flip_factor*(-1./6.)*(((ir+3)-cell_position)-2)**3
+    return s
 
 # -------------------------------
 # Field deposition - linear - rho
