d2e55a20e23a4216fa796661ebf45f580c137d40
Edoardo Zoni <ezoni@lbl.gov>
2020-09-09 10:47:50 -0700

    Read order of finite stencil in z from field solver

--- fbpic/picmi/simulation.py
+++ fbpic/picmi/simulation.py
@@ -65,7 +65,8 @@ class Simulation( PICMI_Simulation ):
         self.fbpic_sim = FBPICSimulation(
             Nz=int(grid.nz), zmin=grid.zmin, zmax=grid.zmax,
             Nr=int(grid.nr), rmax=grid.rmax, Nm=grid.n_azimuthal_modes,
-            dt=dt, use_cuda=True, smoother=smoother, n_order=32,
+            dt=dt, use_cuda=True, smoother=smoother,
+            n_order=self.solver.stencil_order[-1],
             boundaries={'z':grid.bc_zmax, 'r':grid.bc_rmax} )
 
         # Set the moving window
