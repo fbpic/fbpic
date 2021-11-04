08d96f6268d836f1834d0e75932eb3e718b5cd64
Manuel Kirchen <manuel.kirchen@desy.de>
2021-11-04 08:12:57 +0100

    Temporarily remove ParaxialApproximationLaser class

--- fbpic/lpa_utils/laser/__init__.py
+++ fbpic/lpa_utils/laser/__init__.py
@@ -5,17 +5,9 @@ It imports functions which are useful when initializing a laser pulse
 from .laser import add_laser, add_laser_pulse
 from .laser_profiles import GaussianLaser, LaguerreGaussLaser, \
               DonutLikeLaguerreGaussLaser, FlattenedGaussianLaser, \
-              FewCycleLaser, ParaxialApproximationLaser
-from .longitudinal_laser_profiles import GaussianChirpedLongitudinalProfile
-from .transverse_laser_profiles import GaussianTransverseProfile, \
-    LaguerreGaussTransverseProfile, DonutLikeLaguerreGaussTransverseProfile, \
-    FlattenedGaussianTransverseProfile
+              FewCycleLaser
 
 __all__ = ['add_laser', 'add_laser_pulse',
             'GaussianLaser', 'LaguerreGaussLaser',
             'DonutLikeLaguerreGaussLaser', 'FlattenedGaussianLaser',
-            'FewCycleLaser', 'ParaxialApproximationLaser',
-            'GaussianChirpedLongitudinalProfile', 'GaussianTransverseProfile',
-            'LaguerreGaussTransverseProfile',
-            'DonutLikeLaguerreGaussTransverseProfile',
-            'FlattenedGaussianTransverseProfile']
+            'FewCycleLaser']
