ced2d1817caa881b12faf3e4640382bc9749e2e3
Remi Lehe <remi.lehe@normalesup.org>
2022-09-18 19:20:54 -0700

    Add runtime error for numba 0.56.0

--- fbpic/utils/cuda.py
+++ fbpic/utils/cuda.py
@@ -8,7 +8,8 @@ It defines a set of generic functions that operate on a GPU.
 import warnings
 import numba
 numba_version = (int(numba.__version__.split('.')[0]),
-                 int(numba.__version__.split('.')[1]))
+                 int(numba.__version__.split('.')[1]),
+                 int(numba.__version__.split('.')[2]))
 from numba import cuda
 import numpy as np
 
@@ -386,6 +387,10 @@ if cuda_installed:
             # numba kernel
             module = cupy.cuda.function.Module()
             if numba_version[1] >= 56:
+                if numba_version[2] == 0:
+                    raise RuntimeError(
+                        'FBPIC is incompatible with numba 0.56.0.\n'
+                        'Please install either a later or an earlier version.')
                 kernel = next(iter(numba_kernel.overloads.values()))
                 ptx = kernel._codelibrary.get_asm_str()
                 module.load(bytes(ptx, 'UTF-8'))
