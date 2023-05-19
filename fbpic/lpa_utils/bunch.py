0cc56d004be6beff8e3cf0b3dacd82380cabefd1
Remi Lehe <remi.lehe@normalesup.org>
2023-05-19 09:39:23 -0700

    Add option to symmetrize Gaussian beam

--- fbpic/lpa_utils/bunch.py
+++ fbpic/lpa_utils/bunch.py
@@ -127,7 +127,8 @@ def add_particle_bunch_gaussian(sim, q, m, sig_r, sig_z, n_emit, gamma0,
                                 sig_gamma, n_physical_particles,
                                 n_macroparticles, tf=0., zf=0., boost=None,
                                 save_beam=None, z_injection_plane=None,
-                                initialize_self_field=True):
+                                initialize_self_field=True,
+                                symmetrize=False):
     """
     Introduce a relativistic Gaussian particle bunch in the simulation,
     along with its space charge field.
@@ -192,7 +193,18 @@ def add_particle_bunch_gaussian(sim, q, m, sig_r, sig_z, n_emit, gamma0,
     initialize_self_field: bool, optional
        Whether to calculate the initial space charge fields of the bunch
        and add these fields to the fields on the grid (Default: True)
+
+    symmetrize: bool, optional
+       Whether to symmetrize the beam with 4-fold rotational symmetry.
+       (Meaning that, for every macroparticle, 3 other macroparticles
+       that are rotated by +pi/2, +pi, -pi/2 in the transverse plane)
+       This ensures that the beam has exactly 0 initial offset in x and y
     """
+    if symmetrize:
+        assert n_macroparticles%4 == 0, \
+            "When using symmetrize, `n_macroparticles` must be a multiple of 4."
+        n_macroparticles = n_macroparticles//4
+
     # Generate Gaussian gamma distribution of the beam
     if sig_gamma > 0.:
         gamma = np.random.normal(gamma0, sig_gamma, n_macroparticles)
@@ -250,6 +262,14 @@ def add_particle_bunch_gaussian(sim, q, m, sig_r, sig_z, n_emit, gamma0,
         x = x - ux * inv_gamma * c * tf
         y = y - uy * inv_gamma * c * tf
         z = z - uz * inv_gamma * c * tf
+
+    # Add 4-fold copies of the beam by rotating particles by pi/2, pi and -pi/2
+    if symmetrize:
+        x, y, z, ux, uy, uz, w = map( np.concatenate, zip(
+                                    [ x, y, z, ux, uy, uz, w],
+                                    [-y, x, z,-uy, ux, uz, w],
+                                    [-x,-y, z,-ux,-uy, uz, w],
+                                    [ y,-x, z, uy,-ux, uz, w] ) )
 
     # Save beam distribution to an .npz file
     if save_beam is not None:
